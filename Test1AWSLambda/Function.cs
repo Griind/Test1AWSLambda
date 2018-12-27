8b5669bfa68064569f343536685d814687df8c62
Roman Svitukha <roman.svitukha@slalom.com>
2018-12-27 12:57:50 -0600

    added image data check

--- Test1AWSLambda/Function.cs
+++ Test1AWSLambda/Function.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -14,19 +18,44 @@ namespace Test1AWSLambda
     {
         private readonly string[] _supportedImageTypes = new string[] { ".jpg", ".png", ".jpeg" };
         private readonly AmazonS3Client _s3Client;
+
         public Function() => _s3Client = new AmazonS3Client();
 
-        public string[] SupportedImageTypes => _supportedImageTypes;
-
-        public string FunctionHandler(S3Event s3Event, ILambdaContext context)
+        public async Task<string> FunctionHandlerAsync(S3Event s3Event, ILambdaContext context)
         {
             foreach (var record in s3Event.Records)
             {
-                if (SupportedImageTypes.Contains(Path.GetExension()))
+                if (!_supportedImageTypes.Contains(Path.GetExtension(record.S3.Object.Key).ToLower()))
                 {
-
+                    Console.WriteLine(
+                    $"Object {record.S3.Bucket.Name}:{record.S3.Object.Key} is not a supported image type");
+                    continue;
                 }
+                Console.WriteLine(
+                  $"Determining whether image {record.S3.Bucket.Name}:{record.S3.Object.Key} has been compressed");
+
+                var taggingResponse = await _s3Client.GetObjectTaggingAsync(new GetObjectTaggingRequest
+                {
+                    BucketName = record.S3.Bucket.Name,
+                    Key = record.S3.Object.Key
+                });
+
+                if (taggingResponse.Tagging.Any(tag => tag.Key == "Compressed" && tag.Value == "true"))
+                {
+                    Console.WriteLine(
+                        $"Image {record.S3.Bucket.Name}:{record.S3.Object.Key} has already been compressed");
+                    continue;
+                }
+
+                using(var objectResponse = await _s3Client.GetObjectAsync(record.S3.Bucket.Name, record.S3.Object.Key))
+                using(Stream responseStream = objectResponse.ResponseStream)
+                {
+                    Console.WriteLine($"Compressing image {record.S3.Bucket.Name}:{record.S3.Object.Key}");
+                }
+
+                
             }
+            return null;
         }
     }
 }
